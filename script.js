bc2b5b6dc3f701b945a4d8febc1b45f4ac61aa61
ranilakshmi <63279825+ranilakshmi@users.noreply.github.com>
2020-05-07 18:59:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+(function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === 'j') {
-        console.log("GoodBye " + names[i]);
-    } else {
-        console.log("Hi " + names[i]);
-    }
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
   }
-
+}
+})();
